ccc00746162211d3424eecdc132bed487af954a2
G-Nikhil-Das <Godaba.NikhilDas1@ltimindtree.com>
2023-10-23 14:35:20 +0530

    Added url for cors

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 5000
 const app = express()
 import path from 'path';
 const __dirname = path.resolve();
-app.use(cors({credentials:true,origin:'http://localhost:3000'}))
+// app.use(cors({credentials:true,origin:'http://localhost:3000'}))
+app.use(cors({credentials:true,origin:'https://nikhil-blogapplication.netlify.app'}))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static(__dirname + '/uploads'))
